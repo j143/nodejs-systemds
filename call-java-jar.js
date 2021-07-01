2a1eded83f9eebd8cba555c63e5f65b5cbae3978
Janardhan Pulivarthi <j143@protonmail.com>
2021-07-01 05:19:13 +0000

    add lib as work around to #2

--- call-java-jar.js
+++ call-java-jar.js
@@ -1,8 +1,16 @@
 var java = require("java");
+var fs = require("fs");
 var systemds_jar = "systemds-2.1.0.jar";
 java.classpath.push(systemds_jar);
+var depDir = "./systemds-lib";
+var dependencies = fs.readdirSync(depDir);
+ 
+dependencies.forEach(function(dependency){
+    java.classpath.push(depDir + "/" + dependency);
+})
 
-// var DMLScript = java.import('org.apache.sysds.api.DMLScript');
+
+var DMLScript = java.import('org.apache.sysds.api.DMLScript');
 var javaLangSystem = java.import('java.lang.System');
-
+// DMLScript.execute();
 javaLangSystem.out.printlnSync('Hello World');
