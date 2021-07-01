178f6385a3368420824f4a3cb36384cf33320023
Janardhan Pulivarthi <j143@protonmail.com>
2021-07-01 04:01:56 +0000

    run java command sucessfully

--- call-java-jar.js
+++ call-java-jar.js
@@ -1,1 +1,8 @@
-var java = require("java")
+var java = require("java");
+var systemds_jar = "systemds-2.1.0.jar";
+java.classpath.push(systemds_jar);
+
+// var DMLScript = java.import('org.apache.sysds.api.DMLScript');
+var javaLangSystem = java.import('java.lang.System');
+
+javaLangSystem.out.printlnSync('Hello World');
